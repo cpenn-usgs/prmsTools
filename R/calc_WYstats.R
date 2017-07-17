b2208672477f1e3ba77ef2595dfc0232f7e2bd0f
Colin Penn <cpenn@usgs.gov>
2017-07-17 15:43:56 -0600

    WYstats dplyr having issue

--- R/calc_WYstats.R
+++ R/calc_WYstats.R
@@ -1,13 +1,21 @@
-calc_WYstats <- function(data, runoffIndex, segOutflowIndex)
+calc_WYstats <- function(data, runoffIndex, segOutflowIndex,
+                         writeWYstats = FALSE, outFile)
 {
   # add concatinated date and WY
   data$date <- as.Date(paste(data$year, data$month, data$day, sep = "-"))
   data$WY <- smwrBase::waterYear(data$date, numeric = TRUE)
   
+  # extract observed (runoff parameter) and simulated (seg_outflow) data sets
+  data <- data[c("date", "WY", 
+                 paste("runoff", runoffIndex, sep = "_"),
+                 paste("seg_outflow", segOutflowIndex, sep = "_"))]
+  names(data) <- c("date", "WY", "obs", "sim")
   
-  
-  
-  
+  ### NOT WORKING####
+  wystats <- dplyr::summarise(dplyr::group_by(data, WY), 
+                              nse = hydroGOF::NSE(sim, obs), 
+                              rmse = hydroGOF::rmse(sim, obs), 
+                              volE = hydroGOF::VE(sim, obs))
   
   
 }
