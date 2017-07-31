81b1caeab055a9f5b3014b0d371801d7e88658a7
Colin Penn <cpenn@usgs.gov>
2017-07-31 11:17:42 -0600

    added roxygen headers

--- R/calc_WYstats.R
+++ R/calc_WYstats.R
@@ -1,3 +1,28 @@
+#' calc_WYstats
+#' @description Function to calculate NSE, RMSE, and VE goodness-of-fit statistics by WY
+#' @param data A \code{dataframe} output from \code{read_Statvar}
+#' @param runoffIndex Integer, index number of observed gage flow in PRMS input data file
+#' @param segOutflowIndex Integer, index number of segment corresponding to observed gage location
+#' @param writeWYstats Logical, if \code{TRUE} write .csv file of WY stats.
+#' @param outFile Character, pathname and filename of .csv file if \code{writeWYstats = TRUE} 
+#' @details uses the \code{hydroGOF} package for calculating the following statistics by WY:
+#' @details NSE: Nash-Sutcliffe Efficiency
+#' RMSE: Root Mean Square Error
+#' VE: Volumetric Efficiency
+#' See \code{hydroGOF} package for more documentation and references
+#' @examples 
+#' data("exampleData",package="prmsTools")
+#' x <- exampleData
+#' basinStats <- calc_WYstats(data = x, runoffIndex = 6, segOutflowIndex = 9)
+#' @importFrom smwrBase waterYear
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom hydroGOF NSE rmse VE
+#' @seealso \code{\link[prmsTools]{read_Statvar}}, 
+#' \code{\link[hydroGOF]{NSE}}, \code{\link[hydroGOF]{rmse}}, \code{\link[hydroGOF]{VE}}
+#' @export
+#' @return A dataframe containing goodness-of-fit statistics by water year
+
 calc_WYstats <- function(data, runoffIndex, segOutflowIndex,
                          writeWYstats = FALSE, outFile = NULL)
 {
--- R/read_Statvar.R
+++ R/read_Statvar.R
@@ -1,3 +1,16 @@
+#' read_Statvar
+#' @description Function to read the statvar file output by PRMS
+#' @param file Character, pathname to statvar file
+#' @param NAval Integer, value of missing data in statvar/data file (usually -999) 
+#' @details Variables and index numbers of the statvar file are determined by the user in the PRMS control file
+#' prior to simulation. This function reads the statvar header with variables and index number, and outputs a \code{dataframe}
+#' with column titles corresponding to the statvar elements
+#' @examples 
+#' exampleData <- read_Statvar(file = data/rghw_Daymet.statvar)
+#' @seealso \code{\link[prmsTools]{calc_WYstats}}, 
+#' @export
+#' @return A dataframe containing the statvar output with proper column titles
+
 read_Statvar <- function(file, NAval = -999)
 {
   # number of variables is stored in first line of statvar file
