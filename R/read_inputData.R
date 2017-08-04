2c00fb94bf4db91a9741c259824f0f503e0ef4b1
Colin Penn <cpenn@usgs.gov>
2017-08-04 09:06:08 -0600

    add read_inputData roxygen and build

--- R/read_inputData.R
+++ R/read_inputData.R
@@ -1,4 +1,17 @@
-
+#' read_inputData
+#' @description Function to read the input data file used in a PRMS simulation
+#' @param file Character, pathname to input data file
+#' @param nheader Integer, number of header lines, usually containing created by and station metadata, in the file
+#' @param nvars Integer, number of unique variables (ex. runoff, tmin, tmax; nvars=3) in the data file
+#' @param NAval Integer, value of missing data in statvar/data file (usually -999) 
+#' @details Variables and station numbers of the data file are determined by the \code{variable numberOfStations}
+#' which is the format read by PRMS and setup by the user prior to a simulation. This function will assign each
+#' timeseries the column title of variable_stationNumber in the order that they are listed.
+#' @examples
+#' exampleData <- read_inputData(file = "data/rghw_ALL.data")
+#' @seealso \code{\link[prmsTools]{calc_WYstats}}, 
+#' @export
+#' @return A dataframe containing the input data file with varialbe_stationNumber column titles
 
 
 read_inputData <- function(file,
