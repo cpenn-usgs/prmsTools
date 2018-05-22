42275027dbf146c74a9a2dfc9058a1cb73ac59c5
Colin Penn <cpenn@usgs.gov>
2018-05-22 16:37:31 -0600

    prms statvar has blank last column

--- R/read_Statvar.R
+++ R/read_Statvar.R
@@ -23,7 +23,7 @@ read_Statvar <- function(file, NAval = -999)
   
   # read in statvar file and rename columns
   statvar <- read.table(file = file, sep = " ", skip = (nvars+1))
-  names(statvar) <- c("id", "year", "month", "day", "hour", "min", "sec", varNames$vars)
+  names(statvar) <- c("id", "year", "month", "day", "hour", "min", "sec", varNames$vars, 'blank')
   
   # replace no data values with NA
   statvar[statvar == NAval] <- NA
